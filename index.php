2c5b9dc870a1bbe3797ed3c032cbaf4e6904cdbe
muhammadarielramo <if23.muhammadramo@mhs.ubpkarawang.ac.id>
2024-11-29 14:37:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $events = $stmt->fetchAll();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Favicon -->
-    <link rel="icon" href="img/logo_bprotic.png">
+    <link rel="icon" href="img/logo-bprotic.png">
     <title>Landing Page</title>
 
     <style>
